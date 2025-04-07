4d8446ef87a21d1c3c7acd0dc90536ca473c81d4
aersosi <acerelroy@gmail.com>
2025-04-07 12:03:27 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button.tsx";
+
+function App() {
+  return (
+    <div className="flex gap-4">
+      <p>test</p>
+      <p>test2</p>
+        <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
